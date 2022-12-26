c6ff2d8140ca1605767c7745cd4d4209ba980d05
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-12-26 02:41:00 -0600

    Camera zoom lerping

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -3,13 +3,13 @@ namespace Project2D;
 public partial class CameraController : Camera2D
 {
 	private float ZoomIncrement { get; set; } = 0.02f;
-	private float ZoomLevel { get; set; }
+	private float TargetZoom { get; set; }
 	private float MinZoom { get; set; } = 0.01f;
 	private float MaxZoom { get; set; } = 1.0f;
 
 	public override void _Ready()
 	{
-		ZoomLevel = Zoom.x;
+		TargetZoom = Zoom.x;
 	}
 
 	public override void _Process(double delta)
@@ -32,6 +32,11 @@ public partial class CameraController : Camera2D
 		Position += velocity.Normalized() * speed;
 	}
 
+	public override void _PhysicsProcess(double delta)
+	{
+		Zoom = Lerp(Zoom, new Vector2(TargetZoom, TargetZoom), 0.25f);
+	}
+
 	public override void _UnhandledInput(InputEvent @event)
 	{
 		if (@event is not InputEventMouseButton mouseButton)
@@ -41,13 +46,14 @@ public partial class CameraController : Camera2D
 			return;
 
         if (mouseButton.ButtonIndex == MouseButton.WheelUp)
-			ZoomLevel += ZoomIncrement;
+			TargetZoom += ZoomIncrement;
 
         if (mouseButton.ButtonIndex == MouseButton.WheelDown)
-			ZoomLevel -= ZoomIncrement;
+			TargetZoom -= ZoomIncrement;
 
-		ZoomLevel = Mathf.Clamp(ZoomLevel, MinZoom, MaxZoom);
-
-		Zoom = new Vector2(ZoomLevel, ZoomLevel);
+		TargetZoom = Mathf.Clamp(TargetZoom, MinZoom, MaxZoom);
 	}
+
+	private Vector2 Lerp(Vector2 v1, Vector2 v2, float t) =>
+		new Vector2(Mathf.Lerp(v1.x, v2.x, t), Mathf.Lerp(v1.y, v2.y, t));
 }
