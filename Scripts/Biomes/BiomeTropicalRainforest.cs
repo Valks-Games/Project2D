2fcd7c9f5b7ec0173ba5aa16f623dd72be9355a4
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-12-27 00:18:20 -0600

    simplify rainforest for now

--- Scripts/Biomes/BiomeTropicalRainforest.cs
+++ Scripts/Biomes/BiomeTropicalRainforest.cs
@@ -3,23 +3,25 @@
 public class BiomeTropicalRainForest : Biome
 {
 	private World World { get; set; }
-	private float[,] Noise { get; set; }
+	//private float[,] Noise { get; set; }
 
 	public BiomeTropicalRainForest(World world)
 	{
 		World = world;
-		Noise = World.CalcNoise(0.02f);
+		//Noise = World.CalcNoise(0.02f);
 	}
 
 	public override void Generate(int x, int z)
 	{
-		var noiseValue = Noise[x, z];
+		/*var noiseValue = Noise[x, z];
 
 		if (noiseValue < 50)
 			World.SetTile(x, z, 4);
 		else if (noiseValue is >= 50 and <= 200)
 			World.SetTile(x, z, 5);
 		else
-			World.SetTile(x, z, 6);
+			World.SetTile(x, z, 6);*/
+
+		World.SetTile(x, z, 4);
 	}
 }
